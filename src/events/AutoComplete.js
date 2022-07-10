cfb9d3b75751f3a3b8c46dcd7a774cbe178e90fa
CuzImClicks <58398364+CuzImClicks@users.noreply.github.com>
2022-07-10 14:06:49 +0200

    added autocomplete to the subscribe commnd

---
+++ src/events/AutoComplete.js
@@ -0,0 +1,27 @@
+import { langProjMap } from "../commands/subscribe.js";
+import { Logger } from "../utils/Logger.js";
+
+const lg = new Logger("AutoComplete", "info");
+
+export default {
+    name: "interactionCreate",
+    execute: async function execute(interaction) {
+        if (!interaction.isAutocomplete()) return;
+
+        const focusedValue = interaction.options.getFocused();
+        switch (interaction.commandName) {
+
+            case "subscribe": {
+                await interaction.respond(
+                    langProjMap.map((lang) => lang.name).filter(choice => choice.startsWith(focusedValue)).map(choice => ({ name: choice, value: choice }))
+                );
+                break;
+            }
+            
+            default: {
+                lg.info(`Unknown AutoComplete`);
+                lg.debug(interaction);
+            }
+        }
+    }
+};
